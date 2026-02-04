0b5bd06ca44da19abab4b11220840bd02e391338
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2026-02-04 18:00:55 +0000

    MAN-1822: update tests

--- server/services/auditService.test.ts
+++ server/services/auditService.test.ts
@@ -45,7 +45,7 @@ describe('Audit service', () => {
       })
 
       expect(hmppsAuditClient.sendMessage).toHaveBeenCalledWith({
-        what: 'PAGE_VIEW_EXAMPLE_PAGE',
+        what: 'PAGE_VIEW_HOME_PAGE',
         who: 'user1',
         subjectId: 'subject123',
         subjectType: 'exampleType',
