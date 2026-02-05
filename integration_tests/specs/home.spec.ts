af8c0f013297eedc9b68c492291716e14e9c2f57
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2026-02-05 18:01:36 +0000

    MAN-1822: update tests

---
+++ integration_tests/specs/home.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test'
+
+import { login, resetStubs } from '../testUtils'
+import HomePage from '../pages/homePage'
+
+test.describe('Home', () => {
+  test.afterEach(async () => {
+    await resetStubs()
+  })
+
+  test('Home page is loaded', async ({ page }) => {
+    await login(page)
+
+    const homePage = await HomePage.verifyOnPage(page)
+
+    await expect(homePage.header).toContainText('Sent reminders')
+  })
+})
