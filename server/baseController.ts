24a4a0f939a249a4300946adf01ec12782ebea58
Ravindra Kumar <ravindrakumar.killamsetty@justice.gov.uk>
2026-05-18 16:22:20 +0100

    MTRSB-78: addressed review comments

--- server/baseController.ts
+++ server/baseController.ts
@@ -2,10 +2,12 @@ import { Request, Response, NextFunction } from 'express'
 import config from './config'
 import { defaultName } from './utils/azureAppInsights'
 
+const applicationInsightsRoleName = defaultName()
+
 const baseController = () => {
   return (req: Request, res: Response, next: NextFunction): void => {
     res.locals.applicationInsightsConnectionString = config.appInsights.connectionString
-    res.locals.applicationInsightsRoleName = defaultName()
+    res.locals.applicationInsightsRoleName = applicationInsightsRoleName
     return next()
   }
 }
