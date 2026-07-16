6ab6545c9c69e5ba86effeeafe726c8db3c964b1
rav-moj <ravindrakumar.killamsetty@justice.gov.uk>
2026-07-16 12:04:24 +0100

    MTRSB-116: Address security alert open Telemetry (#111)
    * MTRSB-116: Address security alert opentelemetry
    
    * MTRSB-115: addressed review comments
    
    * MTRSB-116: Addressed review comments
    
    * MTRSB-116: Addressed review comments

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,11 @@ import promClient from 'prom-client'
 import createApp from './app'
 import { services } from './services'
 import { createMetricsApp } from './monitoring/metricsApp'
+import { buildAppInsightsClient } from './utils/azureAppInsights'
+import applicationInfoSupplier from './applicationInfo'
+
+const applicationInfo = applicationInfoSupplier()
+buildAppInsightsClient(applicationInfo)
 
 promClient.collectDefaultMetrics()
 const app = createApp(services())
